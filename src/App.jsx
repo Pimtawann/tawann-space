7b131131debbacf9e16ec4f82a0b50c716db452c
Pimtawan Wonnawijit <pimtawan@MacBook-Air-khxng-Pimtawan.local>
2025-08-17 23:32:37 +0700

    feat: add HeroSection component and integrate it into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import './index.css'
 import NavBar from './component/NavBar'
+import HeroSection from './component/HeroSection'
 
 function App() {
 
   return (
     <div className='min-h-screen'>
       <NavBar/>
+      <div className='max-w-[768px] md:max-w-[1200px] mx-auto md:px-6'>
+        <HeroSection/>
+      </div>
     </div>
   )
 }
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function NavBar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     return(
-        <nav className="w-full border-b border-gray-300 fixed top-0 left-0 right-0 z-50">
+        <nav className="w-full bg-[#f9f8f6] border-b border-gray-300 fixed top-0 left-0 right-0 z-50">
         <div className="w-full px-7 py-2 md:px-30 md:py-4 flex justify-between items-center">
         {/* logo */}
         <div className="text-2xl md:text-4xl text-[#26231e] flex items-center">hh<span className="text-[#12b279] text-2xl md:text-4xl">.</span></div>
@@ -14,7 +14,7 @@ function NavBar() {
             <button className="text-4xl text-gray-500 md:hidden" onClick={() => setIsMenuOpen((prev) => !prev)}>☰</button>
         </div>
         {isMenuOpen &&(
-        <div className="absolute top-full w-full left-0 flex flex-col items-center gap-7 px-4 py-12 border-b border-gray-300 md:hidden ">
+        <div className="absolute top-full w-full left-0 flex flex-col items-center gap-7 px-4 py-12 border-b bg-[#f9f8f6] border-gray-300 md:hidden ">
             <button className="w-full bg-white border border-black py-3 rounded-full text-lg font-medium mx-auto">Log in</button>
             <button className="w-full bg-[#26231e] text-white py-3 rounded-full text-lg font-medium mx-auto">Sign up</button>
         </div>
