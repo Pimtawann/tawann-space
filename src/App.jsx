6d0d8973d40bc4d2acaab5708e870e9f324fa53b
Pimtawan Wonnawijit <pimtawan@MacBook-Air-khxng-Pimtawan.local>
2025-08-24 22:53:07 +0700

    feat: add ArticleSection component and integrate it into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import './index.css'
 import { NavBar } from './components/NavBar'
 import { HeroSection } from './components/HeroSection'
 import { Footer } from './components/Footer'
+import ArticleSection from './components/ArticleSection'
 
 function App() {
 
   return (
     <div className='min-h-screen'>
       <NavBar/>
-      <div className='max-w-[768px] md:max-w-[1200px] mx-6 md:px-6 md:mx-auto'>
-        <div>
+      <div className='max-w-[768px] md:max-w-[1200px] md:px-6 md:mx-auto'>
+        <div className='mx-6'>
           <HeroSection/>
+        </div>
+        <div>
+          <ArticleSection/>
         </div>
       </div>
       <div>
