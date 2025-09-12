a9ca08f9b9137d31d963f1aa5a2c2e38e4b13c67
Pimtawan wonnawijit <pimtawan.won@hotmail.com>
2025-09-12 15:55:16 +0700

    Revert "Feature/landing page"

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import "./index.css";
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ViewPostPage from "./pages/ViewPostPage";
-import ScrollToTop from "./components/ScrollToTop";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<HomePage />}/>
-          <Route path="/post/:postId" element={<ViewPostPage />}/>
-        </Routes>  
-    </div>
-  );
+    <section>
+      <h1>My First React App on Vite</h1>
+      
+    </section>
+  )
 }
 
-export default App;
+export default App
