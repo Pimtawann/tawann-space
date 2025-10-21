36ba81da40309ca3f5ccce9468a30802ba531278
Pimtawan Wonnawijit <pimtawan@MacBook-Air-khxng-Pimtawan.local>
2025-10-22 01:43:34 +0700

    style: update LoginForm and SignUpForm components with new styles and layout, change font to Poppins, and update page titles

--- src/components/form/LoginForm.jsx
+++ src/components/form/LoginForm.jsx
@@ -4,22 +4,47 @@ import { Link } from "react-router-dom";
 
 export default function LoginForm() {
   return (
-    <div className="w-full max-w-sm mx-auto mt-20 bg-white p-6 rounded-2xl shadow-md">
-      <h1 className="text-2xl font-bold text-center mb-6">Log in</h1>
-      <form className="space-y-4">
-        <Input placeholder="Email" name="email" type="email" />
-        <Input placeholder="Password" name="password" type="password" />
-        <Button className="w-full" type="submit">
-          Log in
-        </Button>
+    <div className="w-full md:w-160 mx-auto mt-20 md:mt-30 bg-brown-2 p-6 rounded-2xl">
+      <h1 className="text-xl font-semibold text-center my-6 text-brown-6">
+        Log in
+      </h1>
+      <form className="space-y-4 md:mx-16">
+        <label htmlFor="email" className="text-brown-4 font-medium">
+          Email
+        </label>
+        <Input
+          placeholder="Email"
+          name="email"
+          type="email"
+          className="bg-white h-12 border border-brown-3 placeholder:text-brown-4 placeholder:font-medium"
+        />
+        <label htmlFor="password" className="text-brown-4 font-medium">
+          Password
+        </label>
+        <Input
+          placeholder="Password"
+          name="password"
+          type="password"
+          className="bg-white h-12 border border-brown-3 placeholder:text-brown-4 placeholder:font-medium"
+        />
+        <div className="flex justify-center">
+          <Button
+            className="w-30 h-12 md:w-34 bg-brown-6 mt-2 text-lg rounded-full font-medium hover:bg-brown-4 cursor-pointer"
+            type="submit"
+          >
+            Log in
+          </Button>
+        </div>
       </form>
 
-      <p className="text-center text-sm mt-4">
-        Don’t have any account?{" "}
-        <Link to="/signup" className="underline">
+      <div className="flex justify-center items-center gap-3 my-4 md:my-6">
+        <span className="text-center text-brown-4 font-medium">
+          Don't have any account?
+        </span>
+        <Link to="/signup" className="text-black !underline">
           Sign up
         </Link>
-      </p>
+      </div>
     </div>
   );
 }
--- src/components/form/SignUpForm.jsx
+++ src/components/form/SignUpForm.jsx
@@ -4,24 +4,62 @@ import { Link } from "react-router-dom";
 
 export default function SignUpForm() {
   return (
-    <div className="w-full max-w-sm mx-auto mt-20 bg-white p-6 rounded-2xl shadow-md">
-      <h1 className="text-2xl font-bold text-center mb-6">Sign up</h1>
-      <form className="space-y-4">
-        <Input placeholder="Full name" name="name" />
-        <Input placeholder="Username" name="username" />
-        <Input placeholder="Email" name="email" type="email" />
-        <Input placeholder="Password" name="password" type="password" />
-        <Button className="w-full" type="submit">
-          Sign up
-        </Button>
+    <div className="w-full md:w-160 mx-auto mt-20 md:mt-30 bg-brown-2 p-6 rounded-2xl">
+      <h1 className="text-xl font-semibold text-center my-6 text-brown-6">
+        Sign up
+      </h1>
+      <form className="space-y-4 md:mx-16">
+        <label htmlFor="name" className="text-brown-4 font-medium">
+          Name
+        </label>
+        <Input
+          placeholder="Full name"
+          name="name"
+          className="bg-white h-12 border border-brown-3 placeholder:text-brown-4 placeholder:font-medium"
+        />
+        <label htmlFor="username" className="text-brown-4 font-medium">
+          Username
+        </label>
+        <Input
+          placeholder="Username"
+          name="username"
+          className="bg-white h-12 border border-brown-3 placeholder:text-brown-4 placeholder:font-medium"
+        />
+        <label htmlFor="email" className="text-brown-4 font-medium">
+          Email
+        </label>
+        <Input
+          placeholder="Email"
+          name="email"
+          type="email"
+          className="bg-white h-12 border border-brown-3 placeholder:text-brown-4 placeholder:font-medium"
+        />
+        <label htmlFor="password" className="text-brown-4 font-medium">
+          Password
+        </label>
+        <Input
+          placeholder="Password"
+          name="password"
+          type="password"
+          className="bg-white h-12 border border-brown-3 placeholder:text-brown-4 placeholder:font-medium"
+        />
+        <div className="flex justify-center">
+          <Button
+            className="w-30 h-12 md:w-34 bg-brown-6 mt-2 text-lg rounded-full font-medium hover:bg-brown-4 cursor-pointer"
+            type="submit"
+          >
+            Sign up
+          </Button>
+        </div>
       </form>
-
-      <p className="text-center text-sm mt-4">
-        Already have an account?{" "}
-        <Link to="/login" className="underline">
+      <div className="flex justify-center items-center gap-3 my-4 md:my-6">
+        <span className="text-center text-brown-4 font-medium">
+          Already have an account?
+        </span>
+        <Link to="/login" className="text-black !underline">
           Log in
         </Link>
-      </p>
+      </div>
     </div>
   );
 }
