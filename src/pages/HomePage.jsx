0b71fd3ccbea52ecc05803856afb8f87d3164f59
Pimtawan Wonnawijit <pimtawan@MacBook-Air-khxng-Pimtawan.local>
2025-10-30 01:28:49 +0700

    refactor: replace AdminNavbar with NavbarController in HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
 import HeroSection from "@/components/HeroSection";
 import Footer from "@/components/Footer";
 import ArticleSection from "../components/ArticleSection";
-import AdminNavbar from "@/components/navbar/AdminNavbar";
+import NavbarController from "@/components/navbar/NavbarController";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col">
-      <AdminNavbar />
+      <NavbarController />
       
       <div className="flex-grow">
         <div className="max-w-[768px] md:max-w-[1200px] md:px-6 md:mx-auto">
